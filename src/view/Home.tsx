dff6dbd86f3c91eed1628d63b88f8ac707f1a082
Axel-Aranibar <axelaranibar3@gmail.com>
2025-02-05 08:36:01 -0300

    Action Modal Eliminar y Editar

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, CardBody, CardHeader, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@heroui/react";
+import { Button, Card, CardBody, CardHeader, DateInput, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Select, SelectItem, useDisclosure } from "@heroui/react";
 import NavigationButton from "../components/NavigationButton";
 import NavBar from "../components/NavBar";
-import { Link } from "react-router-dom";
-import { SetStateAction, useState } from "react"; 
+
+import {CalendarDate, parseDate} from "@internationalized/date";
 
 const schedule = [
   { day: "Lunes", date: "24/01/24", route: "Sarmiento, Toledano, Constitución, etc" },
@@ -12,8 +12,19 @@ const schedule = [
   { day: "Viernes", date: "28/01/24", route: "Las Heras, San Juan, Mendoza, etc" },
   { day: "Sabado", date: "28/01/24", route: "Las Heras, San Juan, Mendoza, etc" },
 ];
+export const day = [
+  {key: "1", label: "Lunes"},
+  {key: "2", label: "Martes"},
+  {key: "3", label: "Miercoles"},
+  {key: "4", label: "Jueves"},
+  {key: "5", label: "Viernes"},
+  {key: "6", label: "Sabado"},
+
+];
 export default function Home() {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { isOpen: isDeleteOpen, onOpen: onOpenDelete, onOpenChange: onDeleteChange } = useDisclosure();
+  const { isOpen: isEditOpen, onOpen: onOpenEdit, onOpenChange: onEditChange } = useDisclosure();
+
   return (
     <div className="container mx-auto p-4 ">
       <NavBar />
@@ -34,10 +45,10 @@ export default function Home() {
 
               </CardHeader>
                 
-                <Button isIconOnly color="danger" size="sm" variant="faded" onPress={onOpen}>
+                <Button isIconOnly color="danger" size="sm" variant="faded" onPress={onOpenDelete}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash-2"><path d="M3 6h18" /><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" /><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" /><line x1="10" x2="10" y1="11" y2="17" /><line x1="14" x2="14" y1="11" y2="17" /></svg>
                 </Button>
-                <Button isIconOnly color="success" variant="faded" onPress={onOpen}>
+                <Button isIconOnly color="success" variant="faded" onPress={onOpenEdit}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" /><path d="m15 5 4 4" /></svg>
                 </Button>
 
@@ -57,12 +68,12 @@ export default function Home() {
 
       <NavigationButton />
 
-       {/* Modal */}
+       {/* Modal Eliminar */}
        <Modal
         isDismissable={false}
         isKeyboardDismissDisabled={true}
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
+        isOpen={isDeleteOpen}
+        onOpenChange={onDeleteChange}
         placement="center"
         backdrop="blur"
       >
@@ -73,6 +84,64 @@ export default function Home() {
               <ModalBody>
 
               <p>¿Estas seguro que quieres borrar el día?</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger"  onPress={onClose}>
+                  Cerrar
+                </Button>
+                <Button color="primary" variant="bordered" onPress={onClose}>
+                  Aceptar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      {/* Modal Editar */}
+      <Modal
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+        isOpen={isEditOpen}
+        onOpenChange={onEditChange}
+        placement="center"
+        backdrop="blur"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Editar Dia De Reparto</ModalHeader>
+              <ModalBody>
+
+              <p>¿Estas seguro que quieres editar el día?</p>
+
+              <Select
+              size="lg"
+               variant="bordered"
+      
+      items={day}
+      label="Dias Semanal"
+      placeholder="Selecionar Dias"
+    >
+      {(animal) => <SelectItem>{animal.label}</SelectItem>}
+    </Select>
+
+    <DateInput
+   
+        isDisabled
+        defaultValue={parseDate("2024-04-04")}
+        label={"Dia Por Defecto"}
+        placeholderValue={new CalendarDate(1995, 11, 6)}
+      />
+
+              <Input
+      size="lg"
+   
+      placeholder="Escribe los luegares por donde recorres"
+      label="Recorrrido"
+      type="text"
+       pattern="string"
+      variant="bordered"
+    />
               </ModalBody>
               <ModalFooter>
                 <Button color="danger"  onPress={onClose}>
