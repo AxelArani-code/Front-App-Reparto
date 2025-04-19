8f89f89d39d2d8dc4560ec5b1fb523d099327893
Axel-Aranibar <axelaranibar3@gmail.com>
2025-04-19 17:54:43 -0300

    Contactarnos

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
 // src/components/AddUser.tsx
 import React, {  useState } from "react";
-import {Button, Input, Checkbox, Link, Form} from "@heroui/react";
+import {Button, Input, Checkbox, Link, Form, Modal, ModalContent, ModalHeader, ModalBody, useDisclosure} from "@heroui/react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useApi } from "../config/useUnisave";
 const Login = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [isVisible, setIsVisible] = useState(false); // Para mostrar/ocultar contraseña
   const [loading, setLoading] = useState(false); // Estado de carga
   const [message, setMessage] = useState(''); // Mensajes de éxito/error
@@ -47,6 +48,12 @@ console.log("Session ID recuperado:", sessionId);
       setLoading(false);
     }
   };
+  const routerContact=()=>{
+    const gmailLink =
+      'https://mail.google.com/mail/?view=cm&fs=1&to=Axel%20Aranibar%20<axelaranibar3@gmail.com>&su=Hola&body=Hola%20Axel%2C%20me%20gustaría%20ponerme%20en%20contacto%20contigo.';
+    
+    window.location.href = gmailLink;
+  }
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex w-full max-w-sm flex-col gap-4 rounded-large px-8 pb-10 pt-6">
@@ -92,8 +99,48 @@ console.log("Session ID recuperado:", sessionId);
   <Button className="w-full" color="primary" type="submit" isLoading={loading}>
     Ingresar
   </Button>
+  <Button className="w-full" color="success" onPress={onOpen} isLoading={loading}>
+    Registrarme
+  </Button>
   {message && <p className="text-center text-sm">{message}</p>}
 </Form>
+
+
+ {/* Modal beta */}
+ <Modal
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+       
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="center"
+        backdrop="blur"
+      >
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 ">Registrarse</ModalHeader>
+              <ModalBody>
+             
+   <div className="flex flex-col items-center justify-center p-4">
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clipboard-list"><rect width="8" height="4" x="8" y="2" rx="1" ry="1" /><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" /><path d="M12 11h4" /><path d="M12 16h4" /><path d="M8 11h.01" /><path d="M8 16h.01" /></svg>
+          <h1 className="text-3xl font-bold text-foreground">AppRepart-Beta</h1>
+      
+
+        </div>
+<p>Para poder acceder a las funcionalidades de esta plataforma. Debes contactarnos y nosotros te creamos el usuario. </p>
+<p>Para que puedas disfrutar la experiencia y te brindamos la organización de tu reparto y a su vez te facilitamos el día a día además si tienes un problema te puedes comunicar con nosotros y te ayudaremos </p>
+              </ModalBody>
+              <Button size="lg" onPress={routerContact}color="success" >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+                  Contactarnos
+                </Button>
+          
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+     
 </div>
 </div>
   );
